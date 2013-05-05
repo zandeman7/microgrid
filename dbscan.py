153fb35d19999f9e3a204f1b72d33a9ef3896a34
divyakarthikeyan89 <divyak@ischool.berkeley.edu>
2013-05-04 20:27:38 -0700

    Included user fed data with a simple 2d array

--- dbscan.py
+++ dbscan.py
@@ -1,7 +1,7 @@
 print(__doc__)
 
 import numpy as np
-# This needs scikit to be installed
+
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
@@ -9,16 +9,27 @@ from sklearn.preprocessing import StandardScaler
 
 
 ##############################################################################
-# Generate sample data
+# Trying with user fed data
+
+
+X = np.array([[1,1],[1,2],[1,1.1],[4,4],[5,5]], np.int32)
+print type(X)
+
+
+"""
+Random sample generation as in original code
+
+
 centers = [[1, 1], [-1, -1], [1, -1]]
 X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
                             random_state=0)
 
 X = StandardScaler().fit_transform(X)
+"""
 
 ##############################################################################
 # Compute DBSCAN
-db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+db = DBSCAN(eps=0.3, min_samples=1).fit(X)
 core_samples = db.core_sample_indices_
 labels = db.labels_
 
@@ -26,13 +37,13 @@ labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 print('Estimated number of clusters: %d' % n_clusters_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
-      % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
-      % metrics.adjusted_mutual_info_score(labels_true, labels))
+#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+# print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+#print("Adjusted Rand Index: %0.3f"
+      #% metrics.adjusted_rand_score(labels_true, labels))
+#print("Adjusted Mutual Information: %0.3f"
+      #% metrics.adjusted_mutual_info_score(labels_true, labels))
 print("Silhouette Coefficient: %0.3f"
       % metrics.silhouette_score(X, labels))
 
@@ -53,12 +64,20 @@ for k, col in zip(unique_labels, colors):
                             if labels[index] == k]
     for index in class_members:
         x = X[index]
+        print col
+        print index
         if index in core_samples and k != -1:
             markersize = 14
         else:
             markersize = 6
         pl.plot(x[0], x[1], 'o', markerfacecolor=col,
                 markeredgecolor='k', markersize=markersize)
+         
 
 pl.title('Estimated number of clusters: %d' % n_clusters_)
 pl.show()
+print class_members[0]
+
+
+
+
